df765ef30fc7790548fac93f9166327ea79e147c
sasaki yumeko <Yumeko.Sasaki@jp.ricoh.com>
2024-11-05 15:49:17 +0900

    変数を使う例題を追加

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
 #hello, worldを出力
 print("hello, World")
-#Sec1演習
+#Sec2演習
 print('Hello, Python!')
+
+#Sec3
 #数値リテラルの出力
 print(1_000_000)
 #べき乗=2^3
@@ -10,3 +12,13 @@ print(2 ** 3)
 print('こんにちは')
 #エスケープシーケンスがあれば'も出力できる
 print('I\'mfime.')
+#文字列を操作
+print('1'+'1')#=11
+print(len('hello'))#=5
+
+#変数を使う
+#送料こみ代金を出力
+delivary_fee = 500
+print(1000 + delivary_fee)#=1500
+delivary_fee = 250
+print(1000 + delivary_fee)#=1250
