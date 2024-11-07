aab3da8832a9b3c796b3941b97572e5504ebb276
sasaki yumeko <Yumeko.Sasaki@jp.ricoh.com>
2024-11-07 13:13:02 +0900

    リストの操作練習追加

--- Sec4.py
+++ Sec4.py
@@ -16,3 +16,20 @@ print(sum(scores))
 #スライス[以上:未満]
 slice_samples = [1, 2, 3, 4, 5]
 print(slice_samples[1:3])
+
+#リストの操作
+scores = [1, 2, 3, 4, 5]
+# 要素の追加
+scores.append(6)
+print(scores)
+#指定した要素を削除
+scores.remove(6)
+# 末尾の要素を削除
+scores.pop()
+print(scores)
+# インデックス指定削除
+scores.pop(2)
+print(scores)
+# 全ての要素削除
+scores.clear
+print(scores)
